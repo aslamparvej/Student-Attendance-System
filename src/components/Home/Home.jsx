8fc4229dd6e15afbb80cc1911b653d601421a776
Aslam Parvej <79070408+aslamparvej@users.noreply.github.com>
2023-12-12 11:07:10 +0000

    Add Stats

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 import './Home.css';
 
 // import StudentAttendanceSystem from "../StudentAttendanceSystem";
@@ -17,11 +19,37 @@ const Home = () => {
         </div>
         <div className="stats-container">
             <div className="card">
-
+              <div className="card-body">
+                <div className="card-body-content stat-total-no-student">
+                  <p className="card-body-text stat-total-no-student-text">Total Number of Students</p>
+                  <p className="card-body-value stat-total-no-student-value"><Link to="/students">400</Link><sup className="increase-value">+20</sup></p>
+                </div>
+              </div>
             </div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
+            <div className="card">
+              <div className="card-body">
+                <div className="card-body-content stat-total-no-student">
+                  <p className="card-body-text stat-total-no-student-text">Total Number of Teachers</p>
+                  <p className="card-body-value stat-total-no-student-value"><Link to="/teachers">36</Link><sup className="increase-value">+2</sup></p>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-body">
+                <div className="card-body-content stat-total-no-student">
+                  <p className="card-body-text stat-total-no-student-text">Total Number of Staff</p>
+                  <p className="card-body-value stat-total-no-student-value"><Link to="/staff">21</Link><sup className="increase-value">+2</sup></p>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-body">
+                <div className="card-body-content stat-total-no-student">
+                  <p className="card-body-text stat-total-no-student-text">Total Earning</p>
+                  <p className="card-body-value stat-total-no-student-value"><Link to="/earning">$325M</Link><sup className="increase-value">+20M</sup></p>
+                </div>
+              </div>
+            </div>
         </div>
         <div className="graph-stats-container">
             <div className="card card-xl w-2x"></div>
